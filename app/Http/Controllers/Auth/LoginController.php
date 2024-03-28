b45a7922cfaa13276cd2cf1c2c55caa94f32c44d
G.Jonathan <gabrieljcp@outlook.com>
2024-03-28 03:36:20 -0300

    feat: implementação do auth:sanctum

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request) { 
+        $user = User::where('email', $request->email)->first();
+    
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Credenciais inválidas'], 401);
+        }
+    
+        $token = $user->createToken('my-app-token')->plainTextToken;
+    
+        return response()->json(['token' => $token]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,24 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\TasksController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/Tasks/all',  [TasksController::class, 'getAllTasks']);
-Route::get('/Tasks/get/{id}',  [TasksController::class, 'getTasks']);
-Route::post('/Tasks/add',  [TasksController::class, 'createTasks']);
-Route::delete('/Tasks/delete/{id}',  [TasksController::class, 'deleteTasks']);
-Route::put('/Tasks/update/{id}',  [TasksController::class, 'updateTasks']);
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/Tasks/all',  [TasksController::class, 'getAllTasks']);
+    Route::get('/Tasks/get/{id}',  [TasksController::class, 'getTasks']);
+    Route::post('/Tasks/add',  [TasksController::class, 'createTasks']);
+    Route::delete('/Tasks/delete/{id}',  [TasksController::class, 'deleteTasks']);
+    Route::put('/Tasks/update/{id}',  [TasksController::class, 'updateTasks']);
+
+});
 
