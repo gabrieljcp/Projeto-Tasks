1d04e0512c03ace80ce0ce6d519e7c82a3f6e7e2
G.Jonathan <gabrieljcp@outlook.com>
2024-03-29 08:23:31 -0300

    style: melhorias

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,19 +7,10 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
 
     use RegistersUsers;
 
@@ -68,5 +59,18 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function register(Request $request) {
+
+        $validator = $this->validator($request->all());
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = $this->create($request->all());
+
+        return response()->json(['user' => $user], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TasksController;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,7 +11,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/login', [LoginController::class, 'login']);
+Route::post('/loginAPI', [LoginController::class, 'login']);
+Route::post('/registerAPI', [RegisterController::class, 'register']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
